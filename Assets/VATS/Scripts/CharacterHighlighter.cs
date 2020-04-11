e96f75b948afc339225b078b4fb7d1cb1bd03420
Callum Pertoldi <callumhaines98@hotmail.co.uk>
2020-04-11 13:51:35 +0100

    Genericised CharacterZoneHighlighter to MeshHighlighter. Made CharacterHighlighter that uses MeshHighlighter.

---
+++ Assets/VATS/Scripts/CharacterHighlighter.cs
@@ -0,0 +1,65 @@
+﻿#define Kara_Debug
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHighlighter : MeshHighlighter
+{
+    readonly float[] m_zoneVals = { 1.0f, 0.9f, 0.8f, 0.7f, 0.6f, 0.5f };
+    BodyZone m_selectedZone = BodyZone.None;
+
+    public void HighlightBodyZone(BodyZone bodyZone)
+    {
+        m_selectedZone = bodyZone;
+
+        HighlightZone(m_zoneVals[(int)bodyZone]);
+    }
+
+    public enum BodyZone
+    {
+        Head,
+        LeftArm,
+        RightArm,
+        Torso,
+        LeftLeg,
+        RightLeg,
+        None
+    }
+
+    [System.Serializable]
+    private class Zone
+    {
+        public BodyZone BodyZone;
+        public float ZoneValue;
+    }
+
+#if Kara_Debug
+    Array debug_EnumVals = typeof(BodyZone).GetEnumValues();
+    private void OnGUI()
+    {
+        TogglePulse(GUILayout.Toggle(IsPulse, "Pulse"));        
+
+        foreach (var enumVal in debug_EnumVals)
+        {
+            BodyZone cur = (BodyZone)enumVal;
+
+            if (cur == m_selectedZone)
+            {
+                GUI.color = Color.green;
+            }
+            if (GUILayout.Button($"Select {enumVal.ToString()}"))
+            {
+                HighlightBodyZone(cur);
+            }
+            GUI.color = Color.white;
+        }
+    }
+
+#endif
+
+
+    private void Start() => Init();
+    private void Update() => Process();
+}
---
+++ Assets/VATS/Scripts/MeshHighlighter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+
+public abstract class MeshHighlighter : MonoBehaviour
+{
+    [SerializeField] MeshRenderer m_meshRenderer              = default;
+    [SerializeField] Color m_highlightColour                  = Color.green;
+    [Header("Pulse settings")]
+    [SerializeField] bool m_shouldPulse                       = true;
+    [SerializeField] float m_pulseRangeMin                    = 0.7f;
+    [SerializeField] float m_pulseRangeMax                    = 1.0f;
+    [SerializeField] float m_pulseSpeed                       = 10f;
+
+    CachingManager m_cachingManager;
+    Material m_VATsMaterial;
+
+    const float m_precisionAllowence = 0.03f;
+
+
+    protected void Init()
+    {
+        m_VATsMaterial = m_meshRenderer.material;
+
+        m_cachingManager = new CachingManager();
+        m_cachingManager.Init(typeof(ShaderValues), Shader.PropertyToID);
+
+        m_VATsMaterial.SetColor(m_cachingManager[ShaderValues._HighlightColor], m_highlightColour);
+    }
+
+    protected void Process()
+    {
+        if (m_shouldPulse)
+        {
+            float pulseBrightness = Mathf.Lerp(m_pulseRangeMin, m_pulseRangeMax, ((Mathf.Sin(Time.timeSinceLevelLoad * m_pulseSpeed) + 1) * 0.5f));
+            m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._HighlightBrightness], pulseBrightness);
+        }
+    }
+
+    protected void TogglePulse(bool shouldPulse)
+    {
+        m_shouldPulse = shouldPulse;
+        m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._HighlightBrightness], 1.0f);
+    }
+
+    public bool IsPulse { get { return m_shouldPulse; } }
+
+    protected void HighlightZone(float zoneID)
+    {
+        m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._SelectedZoneMin], zoneID - m_precisionAllowence);
+        m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._SelectedZoneMax], zoneID + m_precisionAllowence);
+    }
+
+    private enum ShaderValues
+    {
+        _SelectedZoneMin,
+        _SelectedZoneMax,
+        _HighlightColor,
+        _HighlightBrightness
+    }
+}
